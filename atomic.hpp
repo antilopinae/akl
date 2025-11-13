752c8036abdb8c8ee2dbb272dd4c19b0af1edb2d
Mikhail Baskakov <m.baskakov@f6.ru>
2025-11-13 03:22:27 -0500

    Initialize commit

---
+++ atomic.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdint.h>
+
+#include <boost/type_traits/is_integral.hpp>
+
+namespace ant {
+    namespace detail {
+        template<typename T, bool IsIntegral>
+        class atomic_impl {
+        public:
+
+        };
+
+
+    }
+}
